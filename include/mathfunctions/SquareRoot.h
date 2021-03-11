7fbf1178880ecf8b5f8bacfed030768f073277f5
ClausKlein <claus.klein@arcormail.de>
2021-03-11 18:53:35 +0100

    add quikhacks to fix build a DLL under windows
    SquareRoot_EXPORTS and Addition_EXPORTS are MathFunctions_EXPORTS

--- include/mathfunctions/SquareRoot.h
+++ include/mathfunctions/SquareRoot.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// quickhack for now! CK
+#ifdef SquareRoot_EXPORTS
+#  define MathFunctions_EXPORTS
+#endif
+
 #include <mathfunctions/mathfunctions_export.h>
 
 namespace MathFunctions {
