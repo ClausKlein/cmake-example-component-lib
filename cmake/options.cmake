43348331bdf71350feffb387a2310516b5e47392
ClausKlein <claus.klein@arcormail.de>
2021-03-11 16:52:30 +0100

    enable dynamic library build by default
    cleanup cmake files too
    update some notes

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -9,7 +9,7 @@ endif()
 # Note: same postfix as fmt and spdlog! CK
 set(CMAKE_DEBUG_POSTFIX d)
 
-option(BUILD_SHARED_LIBS "Build shared libraries" NO)
+option(BUILD_SHARED_LIBS "Build shared libraries" YES)
 
 # build the dynamic libraries and executables together at bin directory
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
